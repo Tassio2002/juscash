a802e0fb1b1564acc5a1cfe11c56e3b88fbe909e
Tassio <81255475+Tassio2002@users.noreply.github.com>
2023-11-27 22:33:14 -0300

    ✨ feat: add error toast

---
+++ src/services/showErrorToast.ts
@@ -0,0 +1,16 @@
+import { toast } from "react-toastify";
+
+function notifyError (errorMessage: string) {
+    toast.error(errorMessage, {
+        position: "bottom-left",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+}
+
+export default notifyError
